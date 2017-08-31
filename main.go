3fab5c8baba61d59ada3ed2fe50fc498f622f642
Gerard Braad <me@gbraad.nl>
2017-08-31 15:38:47 +0800

    Added export and key search option

--- main.go
+++ main.go
@@ -1,65 +1,97 @@
 package main
 
 import (
-        "io"
-        "fmt"
-        "os"
-        "bytes"
-        "encoding/binary"
+	"io"
+	"fmt"
+	"flag"
+	"os"
+	"bytes"
+	"strings"
+	"encoding/binary"
 )
 
 type kvp_record struct {
-        Key   [MAX_KEY_SIZE]byte
-        Value [MAX_VALUE_SIZE]byte
+	Key   [MAX_KEY_SIZE]byte
+	Value [MAX_VALUE_SIZE]byte
 }
 
 func (record *kvp_record) GetKey() string {
-        return string(record.Key[:MAX_KEY_SIZE])
+	return strings.Trim(string(record.Key[:MAX_KEY_SIZE]), "\x00")
 }
 
 func (record *kvp_record) GetValue() string {
-        return string(record.Value[:MAX_VALUE_SIZE])
+	return strings.Trim(string(record.Value[:MAX_VALUE_SIZE]), "\x00")
 }
 
 const (
-        MAX_KEY_SIZE   = 512
-        MAX_VALUE_SIZE = 2048
+	MAX_KEY_SIZE   = 512
+	MAX_VALUE_SIZE = 2048
+	DEFAULT_POOLNAME = "/var/lib/hyperv/.kvp_pool_0"
+	READ_FORMAT = "Key: %s, Value: %s\n"
+	EXPORT_FORMAT = "export %s=%s\n"
 )
 
 func readNextBytes(file *os.File, number int) ([]byte, error) {
-        bytes := make([]byte, number)
+	bytes := make([]byte, number)
 
-        _, err := file.Read(bytes)
-        if err != nil {
-                return nil, err
-        }
+	_, err := file.Read(bytes)
+	if err != nil {
+		return nil, err
+	}
 
-        return bytes, nil
+	return bytes, nil
+}
+
+func getKvpRecords() []kvp_record {
+	file, err := os.Open(DEFAULT_POOLNAME)
+	if err != nil {
+		fmt.Println("Error opening pool")
+		os.Exit(1)
+	}
+	
+	var records []kvp_record
+
+	for {
+		record := kvp_record{}
+		data, err := readNextBytes(file, MAX_KEY_SIZE + MAX_VALUE_SIZE)
+		buffer := bytes.NewBuffer(data)
+		err = binary.Read(buffer, binary.LittleEndian, &record)
+		if err == io.EOF {
+			break
+		}
+
+		records = append(records, record)
+	}
+	
+	return records
+}
+
+func getKvpRecordByKey(key string) {
+	for _, record := range getKvpRecords() {
+		if(strings.EqualFold(record.GetKey(), key)) {
+			fmt.Printf(record.GetValue())
+			break
+		}
+	}
+}
+
+func getAllKvpRecords(format string) {
+	for _, record := range getKvpRecords() {
+		fmt.Printf(format, record.GetKey(), record.GetValue())
+	}
 }
 
 func main() {
-        poolName := "/var/lib/hyperv/.kvp_pool_0"
+	exportMode := flag.Bool("export", false, "Return all for export as environment variable")
+    searchMode := flag.String("key", "", "Search for a specific key and return value")
+	flag.Parse()
 
-        file, err := os.Open(poolName)
-        if err != nil {
-                panic("Oops")
-        }
-
-        var records []kvp_record
-
-        for {
-                record := kvp_record{}
-                data, err := readNextBytes(file, MAX_KEY_SIZE + MAX_VALUE_SIZE)
-                buffer := bytes.NewBuffer(data)
-                err = binary.Read(buffer, binary.LittleEndian, &record)
-                if err == io.EOF {
-                        break
-                }
-
-                records = append(records, record)
-        }
-
-        for _, record := range records {
-                fmt.Printf("Key: %s, Value: %s\n", record.GetKey(), record.GetValue())
-        }
+    if(*searchMode != "") {
+    	getKvpRecordByKey(*searchMode)
+    } else if(*exportMode) {
+    	getAllKvpRecords(EXPORT_FORMAT)
+    } else {
+    	getAllKvpRecords(READ_FORMAT)
+    }
+    os.Exit(0)
 }
